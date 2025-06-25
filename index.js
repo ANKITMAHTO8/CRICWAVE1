91a878400e02f30ffb4a1fa29eed5843ac9e4baf
ANKITMAHTO8 <96264231+ANKITMAHTO8@users.noreply.github.com>
2025-06-25 18:53:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LiveScoreboard from './LiveScoreboard';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LiveScoreboard />
+    <App />
   </React.StrictMode>
 );
