97a24dc8330e3f169d0349661f757a66a645717e
ANKITMAHTO8 <96264231+ANKITMAHTO8@users.noreply.github.com>
2025-06-25 19:21:24 +0530

    Add files via upload

---
+++ cricket-score.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Cricket Score
+Plugin URI: https://wordpress.org/plugins/ocricket-score/
+Description: Get the latest Cricket Live Score for your website.
+Version: 2.0.1
+Author: Cricket scores
+Author URI: https://fscore.net/cricket
+License: GPLv2
+*/
+
+include_once('src/connectFscore.php');
+include_once('src/FscoreConfig.php');
+
+FscoreConfig::getInstance();
+add_shortcode( 'cricket_score', 'connectFscore' );
+
+?>
